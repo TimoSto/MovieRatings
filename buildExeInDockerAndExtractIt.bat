b239b1382d279fe4b706976892feb3d3427d5b5e
Timo Stovermann <timo.stovermann@d-velop.de>
2021-04-23 15:17:51 +0200

    umbenannt

--- buildExeInDockerAndExtractIt.bat
+++
@@ -1,5 +0,0 @@
-docker build -f databaseConnector/Dockerfile -t gotmdbsql .
-docker create -ti --name dummy gotmdbsql bash
-mkdir out
-docker cp dummy:/go/databaseConnector/SaveTrendsOfWeek.exe ./out/SaveTrendsOfWeek.exe
-docker rm -f dummy

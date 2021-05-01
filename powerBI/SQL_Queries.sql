ac43f08520abdd0d400525f7711829eb3e71d7b6
Timo Stovermann <timo.stovermann@d-velop.de>
2021-05-01 17:00:57 +0200

    Query in Liste eingetragen

--- powerBI/SQL_Queries.sql
+++ powerBI/SQL_Queries.sql
@@ -1,3 +1,6 @@
 /*
  Hier können wir die SQL-Queries, die wir dann für PowerBI nutzen, sammeln
  */
+
+/* Covid Differenzen zum vorherigen Monat) */
+select weekNr, cases-LAG(cases) OVER (ORDER BY weekNr ASC), deaths-LAG(deaths) OVER (ORDER BY weekNr ASC), recovered-LAG(recovered) OVER (ORDER BY weekNr ASC) from covid

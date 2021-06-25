06d4a06348a91c7c6be342dfc52b783b0885622d
JuliaGroen <82214504+JuliaGroen@users.noreply.github.com>
2021-06-25 11:53:31 +0200

    Create wochentage.sql

---
+++ database/wochentage.sql
@@ -0,0 +1,7 @@
+INSERT INTO wochentage VALUES(0, 'Montag');
+INSERT INTO wochentage VALUES(1, 'Dienstag');
+INSERT INTO wochentage VALUES(2, 'Mittwoch');
+INSERT INTO wochentage VALUES(3, 'Donnerstag');
+INSERT INTO wochentage VALUES(4, 'Freitag');
+INSERT INTO wochentage VALUES(5, 'Samstag');
+INSERT INTO wochentage VALUES(6, 'Sonntag');

6f23b47abab73e0a4dc4ceb5591484d6eafa9036
Timo Stovermann <timo.stovermann@d-velop.de>
2021-07-03 16:20:38 +0200

    build.bat angepasst

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 docker build -f saveTrendsToDB/Dockerfile -t gotmdbsql .
 docker run -d -ti --name dummy gotmdbsql
 docker cp dummy:/tmp/SaveTrendsOfWeek.exe out/SaveTrendsOfWeek.exe
+docker cp dummy:/tmp/DBHandler.exe out/DBHandler.exe
 docker stop dummy
 docker rm dummy
